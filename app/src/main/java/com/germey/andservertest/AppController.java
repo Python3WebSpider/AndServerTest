537ffa9ffe89eadf3cd2d124910e4aed11935acc
Germey <cqc@cuiqingcai.com>
2021-06-26 07:49:33 +0800

    update controller

--- app/src/main/java/com/germey/andservertest/AppController.java
+++ app/src/main/java/com/germey/andservertest/AppController.java
@@ -2,28 +2,24 @@ package com.germey.andservertest;
 
 
 import com.goldze.mvvmhabit.utils.NativeUtils;
-import com.yanzhenjie.andserver.annotation.Controller;
 import com.yanzhenjie.andserver.annotation.GetMapping;
-import com.yanzhenjie.andserver.framework.body.JsonBody;
-import com.yanzhenjie.andserver.http.HttpRequest;
-import com.yanzhenjie.andserver.http.HttpResponse;
+import com.yanzhenjie.andserver.annotation.QueryParam;
+import com.yanzhenjie.andserver.annotation.RestController;
 
 import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 
-@Controller
+@RestController
 public class AppController {
+
     @GetMapping("/encrypt")
-    public void encrypt(HttpRequest request, HttpResponse response) {
-        String plaintext = request.getQuery("plaintext");
-        int offset = Integer.parseInt(Objects.requireNonNull(request.getQuery("offset")));
-        Map<String, String> sign_map = new HashMap<>();
+    public JSONObject login(@QueryParam("plaintext") String plaintext,
+                            @QueryParam("offset") int offset) {
+        Map<String, String> map = new HashMap<>();
         String sign = NativeUtils.encrypt(plaintext, offset);
-        sign_map.put("sign", sign);
-        JSONObject jsonObject = new JSONObject(sign_map);
-        response.setBody(new JsonBody(jsonObject));
+        map.put("sign", sign);
+        return new JSONObject(map);
     }
 }
